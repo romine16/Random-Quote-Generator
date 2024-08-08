2ac4bf42f108a038cffa2d37db56feeb6938c390
lromine16 <140551459+lromine16@users.noreply.github.com>
2024-08-08 08:05:27 -0500

    added random and print functions

--- js/script.js
+++ js/script.js
@@ -11,18 +11,56 @@ project 1 - A Random Quote Generator
  * `quotes` array 
 ***/
 
-
+const quotes = [
+  {
+    quote: `You know you're in love when you can't fall asleep because reality is finally better than your dreams.`, 
+    source: 'Dr. Seuss',
+    citation: 'goodreads'
+  },
+  {
+    quote:'You only live once, but if you do it right, once is enough.',
+    source:'Mae West'
+  },
+  {
+    quote:'Great leaders are almost always great simplifiers, who can cut through argument, debate, and doubt to offer a solution everybody can understand.', 
+    source:'Colin Powell, US general and statesman'
+  },
+  {
+    quote: `You miss 100 percent of the shots you don't take.`, 
+    source:'Wayne Gretzky',
+    citation: 'The Hockey News',
+    year: 1983
+  },
+  {
+    quote:'Life is like riding a bicycle. To keep your balance, you must keep moving.',
+    source:'Albert Einstein',
+    year: 1930
+  }
+];
 
 /***
  * `getRandomQuote` function
 ***/
 
 
+function getRandomQuote() {
+  return Math.floor(Math.random() * quotes.length) 
+}
+
 
 /***
  * `printQuote` function
 ***/
 
+
+function printQuote() {
+  const randomIndex = getRandomQuote();
+  const randomQuote = `${quotes[randomIndex].quote}`;
+  let html = `<p class="quote">${randomQuote}</p>
+              <p class="source">quote source </p>`
+
+  document.getElementById('quote-box').innerHTML = html;
+}
 
 
 /***
@@ -31,3 +69,4 @@ project 1 - A Random Quote Generator
 ***/
 
 document.getElementById('load-quote').addEventListener("click", printQuote, false);
+document.addEventListener('DOMContentLoaded', function() { printQuote();});
